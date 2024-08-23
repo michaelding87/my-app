c284bccd628e6a2bac22de10e2cf2e00b88bb8e8
Michael Ding <michaelding2004@gmail.com>
2024-08-23 19:28:34 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b0bc68a35dc6a157fc87be21cff9a2fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559b3af9dc7c80359864f8690c0fdb64",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "fe227dc7d4e9bd4d372b",
+    "url": "/my-app/static/css/2.df7079b7.chunk.css"
+  },
+  {
+    "revision": "b49e4d2a94dc8b747745",
+    "url": "/my-app/static/css/main.bd66cfe2.chunk.css"
+  },
+  {
+    "revision": "fe227dc7d4e9bd4d372b",
+    "url": "/my-app/static/js/2.3ec974df.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/my-app/static/js/2.3ec974df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec588937780289f735e",
+    "url": "/my-app/static/js/3.d9f823b6.chunk.js"
+  },
+  {
+    "revision": "b49e4d2a94dc8b747745",
+    "url": "/my-app/static/js/main.78af48ae.chunk.js"
+  },
+  {
+    "revision": "80106bd0f942ca646bfa",
+    "url": "/my-app/static/js/runtime-main.6e5d167b.js"
+  },
+  {
+    "revision": "b5e27cb5818d5c008570a8f49b05b730",
+    "url": "/my-app/static/media/icon_black.b5e27cb5.png"
+  },
+  {
+    "revision": "aa21d2b082006b04074b31547fd0d669",
+    "url": "/my-app/static/media/icon_blue.aa21d2b0.png"
+  },
+  {
+    "revision": "cea4c74a3bcaacef54f39da974363fe8",
+    "url": "/my-app/static/media/icon_grey.cea4c74a.png"
+  },
+  {
+    "revision": "0845cfbcd0de2aef70d9e0dc61c2abb1",
+    "url": "/my-app/static/media/icon_red.0845cfbc.png"
+  }
+]);
